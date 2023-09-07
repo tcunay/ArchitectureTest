18b0065b12906b76612f3365ab00fbc57f4fd3c0
Gevorg Gulkanyan <gulkanyangk@mail.ru>
2023-09-07 14:35:37 +0300

    FIx

---
+++ Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
@@ -0,0 +1,49 @@
+﻿using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+    public class AgentMoveToPlayer : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _agent;
+
+        private Transform _heroTransform;
+        private IGameFactory _gameFactory;
+        
+        private const float MinimalDistance = 1;
+
+        private void Start()
+        {
+            _gameFactory = AllServices.Container.Single<IGameFactory>();
+
+            if (_gameFactory.HeroGameObject != null) 
+                InitHeroTransform();
+            else
+            {
+                _gameFactory.HeroCreated += HeroCreated;
+            }
+        }
+
+        private void Update()
+        {
+            if (HeroIsInit() && HeroNotReached())
+            {
+                _agent.destination = _heroTransform.position;
+            }
+        }
+
+        private bool HeroIsInit() => 
+            _heroTransform != null;
+
+        private void HeroCreated() => 
+            InitHeroTransform();
+
+        private void InitHeroTransform() => 
+            _heroTransform = _gameFactory.HeroGameObject.transform;
+
+        private bool HeroNotReached() => 
+            Vector3.Distance(_agent.transform.position, _heroTransform.transform.position) >= MinimalDistance;
+    }
+}
---
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class Aggro : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver _triggerObserver;
+        [SerializeField] private AgentMoveToPlayer _follow;
+        [SerializeField] private float _coolDown;
+        
+        private Coroutine _aggroCoroutine;
+        private bool _hasAggroTarget;
+
+        private void Start()
+        {
+            _triggerObserver.TriggerEnter += TriggerEnter;
+            _triggerObserver.TriggerExit += TriggerExit;
+
+            SwitchFollowOff();
+        }
+
+        private void OnDestroy()
+        {
+            _triggerObserver.TriggerEnter -= TriggerEnter;
+            _triggerObserver.TriggerExit -= TriggerExit;
+        }
+
+        private void TriggerEnter(Collider obj)
+        {
+            if (_hasAggroTarget == false)
+            {
+                _hasAggroTarget = true;
+                
+                StopAggroCoroutine();
+                SwitchFollowOn();
+            }
+        }
+
+        private void TriggerExit(Collider obj)
+        {
+            if (_hasAggroTarget)
+            {
+                _hasAggroTarget = false;
+                
+                _aggroCoroutine = StartCoroutine(SwitchFollowAfterCooldown());
+            }
+        }
+
+        private void StopAggroCoroutine()
+        {
+            if (_aggroCoroutine != null)
+            {
+                StopCoroutine(_aggroCoroutine);
+                _aggroCoroutine = null;
+            }
+        }
+
+        private IEnumerator SwitchFollowAfterCooldown()
+        {
+            yield return new WaitForSeconds(_coolDown);
+            SwitchFollowOff();
+        }
+
+        private void SwitchFollowOn() => 
+            _follow.enabled = true;
+
+        private void SwitchFollowOff() => 
+            _follow.enabled = false;
+    }
+}
